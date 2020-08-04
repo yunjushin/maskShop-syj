28d016b9919ae5d7ec3c2f6e03f535a955e31b07
yunjushin <yunjus0922@naver.com>
2020-08-04 18:50:21 +0900

    url changed for aws

--- product/src/main/java/maskShop3/external/InventoryService.java
+++ product/src/main/java/maskShop3/external/InventoryService.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="inventory", url="http://localhost:8085")
+//@FeignClient(name="inventory", url="http://localhost:8085")
+@FeignClient(name="inventory", url="http://inventory:8080")
 public interface InventoryService {
 
     @RequestMapping(method= RequestMethod.POST, path="/inventories")
